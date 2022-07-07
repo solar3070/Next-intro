78c6c1b942ac109c31cd4f8178c5d6486853b880
HyeJun <solar3070@naver.com>
2022-07-08 04:39:03 +0900

    custom app component

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,7 @@ import NavBar from "../components/NavBar";
 export default function Home() {
   return (
     <div>
-      <NavBar />
       <h1>Hello</h1>
-      <style jsx global>{`
-        a {
-          color: white;
-        }
-      `}</style>
     </div>
   );
 }
